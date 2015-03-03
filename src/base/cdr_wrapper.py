380ff3249913ea7ea7cfe579b7df9620aeae9603
Sapronov Alexander <sapronov.alexander92@gmail.com>
2015-03-03 12:02:28 +0600

    Fix import

--- src/base/cdr_wrapper.py
+++ src/base/cdr_wrapper.py
@@ -2,11 +2,38 @@
 
 
 import ctypes
-import DLFCN
 import sys
 
-old_dlopen_flags = sys.getdlopenflags()
-sys.setdlopenflags(old_dlopen_flags | DLFCN.RTLD_GLOBAL)
+try:
+    orig = sys.getdlopenflags()
+except AttributeError:
+    from OpenSSL import crypto
+else:
+    try:
+        import DLFCN
+    except ImportError:
+        try:
+            import dl
+        except ImportError:
+            try:
+                import ctypes
+            except ImportError:
+                flags = 2 | 256
+            else:
+                flags = 2 | ctypes.RTLD_GLOBAL
+                del ctypes
+        else:
+            flags = dl.RTLD_NOW | dl.RTLD_GLOBAL
+            del dl
+    else:
+        flags = DLFCN.RTLD_NOW | DLFCN.RTLD_GLOBAL
+        del DLFCN
+
+    sys.setdlopenflags(flags)
+    from OpenSSL import crypto
+
+    sys.setdlopenflags(orig)
+    del orig, flags
 
 
 class CdrWrapper(object):
