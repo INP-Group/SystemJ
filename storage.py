394121d9fa02418225b6d9c3707e9942d714569f
Sapronov Alexander <sapronov.alexander92@gmail.com>
2015-03-25 10:03:48 +0600

    Clean

--- storage.py
+++ storage.py
@@ -2,10 +2,12 @@
 
 from src.storage.storage import Storage
 
+
 def start():
     storage = Storage()
     storage.start()
     print("Server is stopped...")
+
 
 if __name__ == '__main__':
     start()
