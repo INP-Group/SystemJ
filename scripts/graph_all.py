28e64376b86bce03ea4b126c1d89a5236450ea41
Sapronov Alexander <sapronov.alexander92@gmail.com>
2015-04-03 12:13:40 +0600

    Обновил создание графиков

--- scripts/graph_all.py
+++ scripts/graph_all.py
@@ -40,23 +40,30 @@ def get_data_from_storage(channels, time_start, time_end):
 def show_plot(data, name):
     x_val = data[0]
     y_val = data[1]
+    # plt.hold(False)
     plt.title(name)
     plt.xlabel('Time')
     plt.ylabel('Values')
     plt.plot(x_val, y_val)
     plt.plot(x_val, y_val, 'or')
     plt.savefig(os.path.join(RES_FOLDER, 'plots', '%s.png' % name))
-    plt.show()
+    plt.clf()
+    plt.cla()
+    # plt.show()
 
 
 def prepare_data(data):
     times = []
     values = []
-    prev_time = data[0][0]
-    prev_value = data[1][0]
+    prev_time = data[0][0] if isinstance(data[0][0], datetime.datetime) else None
+    prev_value = data[0][1] if isinstance(data[0][1], (float, int)) else None
+
+    assert prev_value is not None
+    assert prev_time is not None
+
     for x in data:
-        cur_time = x[0]
-        cur_value = x[1]
+        cur_time = x[0] if isinstance(x[0], datetime.datetime) else x[1]
+        cur_value = x[0] if isinstance(x[1], datetime.datetime) else x[1]
         if prev_time < cur_time:
             delta = cur_time - prev_time
             for t in xrange(1, int(delta.total_seconds())):
@@ -72,7 +79,7 @@ def prepare_data(data):
 
 def main():
     channels = []
-    for x in xrange(0, 19):
+    for x in xrange(0, 99):
         channels.append('linvac.vacmatrix.Imes%s' % x)
 
     time_start = datetime.datetime(year=2015,
@@ -85,8 +92,8 @@ def main():
 
     time_end = datetime.datetime(year=2015,
                                  month=4,
-                                 day=1,
-                                 hour=1,
+                                 day=3,
+                                 hour=0,
                                  minute=0,
                                  second=0,
                                  microsecond=0)
@@ -95,12 +102,14 @@ def main():
 
     for key, value in data_of_channels.items():
         try:
-            info_for_plot = prepare_data(value)
+            if value:
+                info_for_plot = prepare_data(value)
 
-            name = '%s_%s_%s' % (key, time_start, time_end)
-            show_plot(info_for_plot, name)
+                name = '%s_%s_%s' % (key, time_start, time_end)
+                show_plot(info_for_plot, name)
         except (TypeError, IndexError) as e:
-            print(key, e)
+            print(key, e, value)
+            raise
         # else:
         #     break
 
