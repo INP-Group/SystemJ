0ab1c7c0aedfa6c7e6c029b371cbceaf29ec2ef9
Sapronov Alexander <sapronov.alexander92@gmail.com>
2015-04-14 11:17:38 +0600

    sql команды для изменения способа хранения названия канала.

--- deploy/sql/tables.sql
+++ deploy/sql/tables.sql
@@ -20,7 +20,7 @@ CREATE TABLE datachannel (
 );
 
 
--- INSERT INTO test_channels (channel_name) VALUES ('test_name1');
+-- INSERT INTO test_channels (channel_name) VALUES ('linthermcan.ThermosM.in0');
 -- INSERT INTO test_channels (channel_name) VALUES ('test_name2');
 -- INSERT INTO test_datachannel (channel_id, time, value) VALUES (4, '2015-02-16 20:10:01.709486', 180.089);
 
