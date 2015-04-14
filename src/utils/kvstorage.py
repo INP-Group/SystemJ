df30d16482f8c039c4763e867078848a78228f7c
Sapronov Alexander <sapronov.alexander92@gmail.com>
2015-04-14 09:22:32 +0600

    Добавил базовый код для kvstorage на основе pickledb

---
+++ src/utils/kvstorage.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+import os
+
+import pickledb
+
+from project.settings import DB_FOLDER
+
+
+def _load(filepath=None, force_dump=True):
+    if filepath is None:
+        filepath = os.path.join(DB_FOLDER, 'kvstorage.db')
+    pickledb.load(filepath, force_dump)
+
+
+def set(key, value):
+    _load().set(key, value)
+
+
+def get(key):
+    return _load().get(key)
+
+
+if __name__ == '__main__':
+    pass
